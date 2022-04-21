c71bb4d65972db9d7cb2b540866c6b85b2592b38
Adexgold90 <diplomatmujeeb@gmail.com>
2022-04-21 04:10:34 +0000

    Delete button not working yet

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,50 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [newTask, setNewTask] = useState("");
+	const [tasks, setTasks] = useState([]);
+
+	function addItem() {
+		if (!newTask) {
+			alert("Add a task!");
+			return;
+		}
+		const task = {
+			id: Math.floor(Math.random() * 1000),
+			value: newTask,
+		};
+
+		setTasks((tasks) => [...tasks, task]);
+		setNewTask("");
+	}
+	function deleteItem(id) {
+		const newArray = tasks.filter((task) => task.id !== id);
+		setNewTask("");
+		console.log(newArray);
+	}
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>My todo List</h1>
+			<input
+				type="text"
+				placeholder="What do you have to do today?"
+				value={newTask}
+				onChange={(e) => setNewTask(e.target.value)}
+			/>
+			<button onClick={() => addItem()}>➕ </button>
+			<ul>
+				{tasks.map((task) => {
+					return (
+						<li key={task.id}>
+							{task.value}
+							<button onClick={() => deleteItem(task.id)}>
+								❌
+							</button>
+						</li>
+					);
+				})}
+			</ul>
 		</div>
 	);
 };
